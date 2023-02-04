ce53f084be8edf31e7b6f4fa983ce653b2181466
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-02-04 16:48:42 +0000

    Update fitms_nf_rearrangements.R

--- bin/fitms_nf_rearrangements.R
+++ bin/fitms_nf_rearrangements.R
@@ -4,19 +4,14 @@ library(signature.tools.lib)
 args = commandArgs(trailingOnly=TRUE)
 sample <- args[1]
 sv <- args[2]
+sigs <-args[3]
+
+sigs <- read.csv(sigs)
 
 ##read in the bedpe file
-sv_bedpe <- read.table(sv,sep = "\t", header = TRUE,stringsAsFactors = FALSE,check.names = FALSE)
-#write.csv(sv_bedpe, paste0(sample,'_sv_bedpe_check.csv'))
-
-#build a catalogue from the bedpe file
-res.cat <- bedpeToRearrCatalogue(sv_bedpe)
-
-df <- data.frame(res.cat$rearr_catalogue)
-write.csv(df, paste0(sample,'_rearrangement_catalogue.csv'))
-#rownames(df) <- df[[1]]
-#df <- subset(df, select=-c(1))
-#names(df)[names(df) == "catalogue"] <- sample
+df <- read.csv(sv)
+rownames(df) <- df$X
+df$X <- NULL
 
 plotRearrSignatures(df,output_file = paste0(sample, "_rearrangement_catalogues.pdf"))
 
@@ -25,10 +20,10 @@ organ = "Breast"
 genome.v  ="hg38"
 
 res <-Fit(catalogues = df, 
-          signatures = getOrganSignatures(organ = organ, typemut = 'rearr',version='latest'),
+          signatures = sigs,
           useBootstrap = TRUE, 
           nboot = 200)
 
 
 #write.csv(res$exposures, 'exposures.tsv', sep='\t')
-plotFit(res, 'rearrangement_sigs_results/')
+plotFit(res, 'rearrangement_sigs_results_vcftobedpe_pr8/')
