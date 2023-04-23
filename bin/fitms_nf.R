ed6c53fa978c95e50c2f42babc862f5f43851f36
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-04-23 10:44:50 +0100

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -51,7 +51,8 @@ write.csv(df, paste0(sample,'_catalogue.csv'))
 res <-FitMS(catalogues = df, 
            organ =organ,
            #exposureFilterType="giniScaledThreshold",
-           rareSignatureTier = "T1",
+           #rareSignatureTier = "T1",
+           minErrorReductionPerc = 20,
            useBootstrap = TRUE, 
            nboot = 200)
 
