4749e51de8c6c92b43bef7760b18defd833b5c8e
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-04-26 11:00:03 +0100

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -52,7 +52,7 @@ res <-FitMS(catalogues = df,
            organ =organ,
            #exposureFilterType="giniScaledThreshold",
            #rareSignatureTier = "T1",
-           minErrorReductionPerc = 20,
+           minErrorReductionPerc = 25,
            useBootstrap = TRUE, 
            nboot = 200)
 
